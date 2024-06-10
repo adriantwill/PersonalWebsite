3f8a46f5716ab8622588ea386b6726752c78d6c5
adriantwill <atwill@asu.edu>
2024-06-09 21:16:49 -0600

    added buttons for teams and weekly games

--- script.js
+++ script.js
@@ -15,10 +15,99 @@ class SpecialHeader extends HTMLElement {
   }
 }
 
-fetch(
-  "https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/4/schedule?season=2024"
-)
-  .then((response) => console.log(response))
-  .catch((error) => console.log(error));
-
 customElements.define("nav-matchup", SpecialHeader);
+
+async function fetchData(api) {
+  try {
+    const response = await fetch(api);
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+    const data = await response.json();
+    const container = document.querySelector(".bracket");
+    let games;
+    if (api == "response.json" || "week1.json") {
+      games = data.events;
+    }
+    for (let i = 0; i < games.length; i++) {
+      const divGroup = document.createElement("div");
+      divGroup.id = "matchup";
+      const homeButton = document.createElement("button");
+      homeButton.id = "home";
+      const awayButton = document.createElement("button");
+      awayButton.id = "away";
+      homeButton.innerText =
+        games[i].competitions[0].competitors[0].team.shortDisplayName;
+      homeButton.style.backgroundImage = `linear-gradient(to left, ${
+        teamColors[games[i].competitions[0].competitors[0].team.displayName][0]
+      } 50%, white 50%)`;
+      homeButton.addEventListener("mouseenter", function () {
+        homeButton.style.color =
+          teamColors[
+            games[i].competitions[0].competitors[0].team.displayName
+          ][1];
+      });
+      homeButton.addEventListener("mouseleave", function () {
+        homeButton.style.color = "black";
+      });
+      awayButton.innerText =
+        games[i].competitions[0].competitors[1].team.shortDisplayName;
+      awayButton.style.backgroundImage = `linear-gradient(to right, ${
+        teamColors[games[i].competitions[0].competitors[1].team.displayName][0]
+      } 50%, white 50%)`;
+      awayButton.addEventListener("mouseenter", function () {
+        awayButton.style.color =
+          teamColors[
+            games[i].competitions[0].competitors[1].team.displayName
+          ][1];
+      });
+      awayButton.addEventListener("mouseleave", function () {
+        awayButton.style.color = "black";
+      });
+      divGroup.appendChild(awayButton);
+      divGroup.appendChild(homeButton);
+      container.appendChild(divGroup);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const teamColors = {
+  "Arizona Cardinals": ["#97233F", "#000000"],
+  "Atlanta Falcons": ["#A71930", "#000000", "#A5ACAF"],
+  "Baltimore Ravens": ["#241773", "#000000", "#9E7C0C"],
+  "Buffalo Bills": ["#00338D", "#C60C30"],
+  "Carolina Panthers": ["#0085CA", "#101820", "#A5ACAF", "#BFC0BF"],
+  "Chicago Bears": ["#0B162A", "#C83803", "#DD4814", "#FFFFFF"],
+  "Cincinnati Bengals": ["#FB4F14", "#000000", "#FFFFFF"],
+  "Cleveland Browns": ["#311D00", "#FF3C00", "#FFFFFF"],
+  "Dallas Cowboys": ["#041E42", "#869397", "#FFFFFF"],
+  "Denver Broncos": ["#FB4F14", "#002244", "#FFFFFF"],
+  "Detroit Lions": ["#0076B6", "#B0B7BC", "#FFFFFF"],
+  "Green Bay Packers": ["#203731", "#FFB81C"],
+  "Houston Texans": ["#03202F", "#A71930", "#FFFFFF"],
+  "Indianapolis Colts": ["#002C5F", "#A5ACAF", "#FFFFFF"],
+  "Jacksonville Jaguars": ["#101820", "#006778", "#D7A22A", "#9F792C"],
+  "Kansas City Chiefs": ["#E31837", "#FFB81C"],
+  "Las Vegas Raiders": ["#000000", "#A5ACAF", "#FFFFFF"],
+  "Los Angeles Chargers": ["#0072CE", "#FFB81C", "#002244", "#FFFFFF"],
+  "Los Angeles Rams": ["#002244", "#FFFFFF", "#B3995D"],
+  "Miami Dolphins": ["#008E97", "#F58220", "#FFFFFF", "#005778"],
+  "Minnesota Vikings": ["#4F2683", "#FFC62F", "#FFFFFF"],
+  "New England Patriots": ["#002244", "#C60C30", "#B0B7BC", "#FFFFFF"],
+  "New Orleans Saints": ["#D3BC8D", "#101820"],
+  "New York Giants": ["#0B2265", "#a71930", "#a5acaf"],
+  "New York Jets": ["#203731", "#FFFFFF"],
+  "Philadelphia Eagles": ["#004C54", "#A5ACAF", "#FFFFFF"],
+  "Pittsburgh Steelers": ["#FFB81C", "#101820"],
+  "San Francisco 49ers": ["#AA0000", "#B3995D", "#FFFFFF"],
+  "Seattle Seahawks": ["#002244", "#69BE28", "#A5ACAF", "#FFFFFF"],
+  "Tampa Bay Buccaneers": ["#D50A0A", "#34302B", "#FF7900", "#FFFFFF"],
+  "Tennessee Titans": ["#0C2340", "#4B92DB", "#FFFFFF"],
+  "Washington Commanders": ["#773141", "#FFB612"],
+};
+
+function getTeamColor(team) {
+  return teamColors[team] || "#000000";
+}
