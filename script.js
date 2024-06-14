4e5d2602963824508670d74d2800dd01a8d3a1fc
adriantwill <atwill@asu.edu>
2024-06-13 21:02:39 -0600

    dropdown menu

--- script.js
+++ script.js
@@ -104,6 +104,30 @@ const teamColors = {
   "Washington Commanders": ["#773141", "#FFB612"],
 };
 
-function getTeamColor(team) {
-  return teamColors[team] || "#000000";
-}
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelector(".menu li");
+  const selected = dropdown.querySelector(".selected");
+  select.addEventListener("click", () => {
+    select.classList.toggle("select-clicked");
+    menu.classList.toggle("menu-open");
+  });
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      selected.innerText = option.innerText;
+      selected.classList.add("text-fade-in");
+      setTimeout(() => {
+        selected.classList.remove("text-fade-in");
+      }, 300);
+      select.classList.remove("select-clicked");
+      menu.classList.remove("menu-open");
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+      option.classList.add("active");
+    });
+  });
+});
