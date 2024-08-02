fb25cb7bc2ab3b6a7ac7d2152a0136ac67d70cd7
adriantwill <atwill@asu.edu>
2024-08-01 22:02:10 -0600

    update

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ class SpecialHeader extends HTMLElement {
 }
 
 class SpecialTable extends HTMLElement {
-  //test
+  //teste
   connectedCallback() {
     this.innerHTML = `
          <table class="standings-table">
